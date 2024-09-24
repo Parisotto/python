757c2f14fac4877b163d9efe07e339d5624e8bf6
Parisotto <parisotto@gmail.com>
2024-09-24 09:00:00 -0300

    Atualização

---
+++ a02_input.py
@@ -0,0 +1,13 @@
+nome = "Asdrubal"
+nome = input("Digite seu nome: ")
+
+print(nome)
+
+n1 = int(input("Digite um número inteiro: "))
+n2 = input("Digite outro número inteiro: ")
+n2 = int(n2)
+print (n1 + n2)
+
+sobreNome = input("Digite seu sobrenome: ")
+idade = input("Digite sua idade: ")
+print(f"Seu nome é {nome} {sobreNome} e sua idade é  {idade}")
