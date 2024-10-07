7b32f641de5ad511a30038a8fb6f44a9dc599d80
Parisotto <parisotto@gmail.com>
2024-10-07 08:33:06 -0300

    .

---
+++ a08a_Exercicios.py
@@ -0,0 +1,9 @@
+'''
+1. Escreva uma função que seja capaz de receber a quantidade de anos e tranformar em meses, dias, horas, minutos e segundos. Saída desejada:
+    2 anos equivalem a:
+        24 meses
+        730 dias
+        17520 horas
+        1051200 minutos
+        63072000 segundos
+'''
