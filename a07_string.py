bf6160e9182a94a3940f2191f8edba393d01710a
Parisotto <parisotto@gmail.com>
2024-10-07 08:00:30 -0300

    .

---
+++ a07_string.py
@@ -0,0 +1,28 @@
+string = "Hello world!"
+# for caracter in string:
+#     print(caracter)
+
+# print(string[6])
+# print(string[7:12])
+#print("Olá, mundo!"[5:10])
+#print("Bom dia!"[-1])
+
+s1 = "Vamos"
+s2 = "la!"
+#print(s1 + " " + s2)
+#print(len("Tamanho de um texto."))
+texto = len("Tamanho de um texto.")
+#print(texto)
+
+mensagem = "Tudo que é sólido se desmancha no ar."
+#print("lid" in mensagem) # True
+#print("y" in mensagem) # False
+
+# Fatiamento
+slicing = mensagem[11:17]
+#print(slicing) # "é sólido"
+
+print(mensagem.upper())
+print(mensagem.lower())
+print(mensagem.replace("o ar", "a atmosfera"))
+print(" Hoje em dia.  ".replace(" ", ""))
