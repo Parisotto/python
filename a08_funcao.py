f683dc8680ebcbbb7de8de16e2f41065b1ced9eb
Parisotto <parisotto@gmail.com>
2024-10-07 08:23:05 -0300

    .

---
+++ a08_funcao.py
@@ -0,0 +1,21 @@
+# função (de)
+
+def hello():
+    print("Olá, mundo!")
+
+def mensagem(msg):
+    return f"Mensagem: {msg}"
+
+def multiplicar(x, y):
+    return x * y
+
+hello()
+print(mensagem("cambio!!!"))
+print(multiplicar(3, 12))
+
+def saudacao(nome, cumpr="Olá"):
+    print(f"{cumpr}, {nome}")
+
+saudacao("Edson")
+saudacao("Parisotto", "Bom dia")
+
